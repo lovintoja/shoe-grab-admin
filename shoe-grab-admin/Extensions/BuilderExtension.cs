ea57e4cc751f299d68802831fdb5d852261d0ec1
Filip Dumanowski <filip.dumanowski@qiagen.com>
2025-02-11 11:11:09 +0100

    xxx-stash

--- shoe-grab-admin/Extensions/BuilderExtension.cs
+++ shoe-grab-admin/Extensions/BuilderExtension.cs
@@ -75,32 +75,14 @@ public static class BuilderExtension
 
     public static void SetupKestrel(this WebApplicationBuilder builder)
     {
+        builder.WebHost.UseKestrel(options =>
+        {
+            options.Configure();
+        });
+
         builder.WebHost.ConfigureKestrel((context, options) =>
         {
             var kestrelSection = context.Configuration.GetSection("Kestrel:Endpoints");
-
-            var grpcEndpoint = kestrelSection.GetSection("Grpc");
-            if (grpcEndpoint.Exists())
-            {
-                var grpcUrl = new Uri(grpcEndpoint["Url"]);
-                options.Listen(IPAddress.Parse(grpcUrl.Host), grpcUrl.Port, listenOptions =>
-                {
-                    listenOptions.Protocols = Enum.Parse<HttpProtocols>(grpcEndpoint["Protocols"]);
-                    listenOptions.UseHttps(httpsOptions =>
-                    {
-                        var certificatePath = grpcEndpoint["Certificate:Path"];
-                        var certificatePassword = grpcEndpoint["Certificate:Password"];
-                        var parseSuccess = Enum.TryParse(grpcEndpoint["Certificate:ClientCertificateMode"], out ClientCertificateMode clientCertificateMode);
-
-                        if (certificatePath != null && certificatePassword != null && parseSuccess)
-                        {
-                            var certificate = new X509Certificate2(certificatePath, certificatePassword);
-                            httpsOptions.ServerCertificate = certificate;
-                            httpsOptions.ClientCertificateMode = clientCertificateMode;
-                        }
-                    });
-                });
-            }
 
             var restApiEndpoint = kestrelSection.GetSection("RestApi");
             if (restApiEndpoint.Exists())
